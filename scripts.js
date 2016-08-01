80d7fd702f002217769d91521cc1d191cebad457
Ray <hjc440@nyu.edu>
2016-08-01 15:06:33 -0400

    new videos file

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 
 //the pause button
-var vid = document.getElementById("bgvid"),
-pauseButton = document.getElementById("vidpause");
-function vidFade() {
-    vid.classList.add("stopfade");
-}
-vid.addEventListener('ended', function() {
-    // only functional if "loop" is removed 
-     vid.pause();
-	// to capture IE10
-	vidFade();
-});
-pauseButton.addEventListener("click", function() {
-    vid.classList.toggle("stopfade");
-	if (vid.paused) {
-vid.play();
-		pauseButton.innerHTML = "Pause";
-	} else {
-        vid.pause();
-        pauseButton.innerHTML = "Paused";
-	}
-})
+// var vid = document.getElementById("bgvid"),
+// pauseButton = document.getElementById("vidpause");
+// function vidFade() {
+//     vid.classList.add("stopfade");
+// }
+// vid.addEventListener('ended', function() {
+//     // only functional if "loop" is removed 
+//      vid.pause();
+// 	// to capture IE10
+// 	vidFade();
+// });
+// pauseButton.addEventListener("click", function() {
+//     vid.classList.toggle("stopfade");
+// 	if (vid.paused) {
+// vid.play();
+// 		pauseButton.innerHTML = "Pause";
+// 	} else {
+//         vid.pause();
+//         pauseButton.innerHTML = "Paused";
+// 	}
+// })

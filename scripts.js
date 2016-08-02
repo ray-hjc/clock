24d4951a3309d4f02bc2b5eafeffba45faaa45cb
Ray <hjc440@nyu.edu>
2016-08-01 23:12:10 -0400

    new videos file

--- scripts.js
+++ scripts.js
@@ -1,23 +1,59 @@
 
-//the pause button
-// var vid = document.getElementById("bgvid"),
-// pauseButton = document.getElementById("vidpause");
-// function vidFade() {
-//     vid.classList.add("stopfade");
-// }
-// vid.addEventListener('ended', function() {
-//     // only functional if "loop" is removed 
-//      vid.pause();
-// 	// to capture IE10
-// 	vidFade();
-// });
-// pauseButton.addEventListener("click", function() {
-//     vid.classList.toggle("stopfade");
-// 	if (vid.paused) {
-// vid.play();
-// 		pauseButton.innerHTML = "Pause";
-// 	} else {
-//         vid.pause();
-//         pauseButton.innerHTML = "Paused";
-// 	}
-// })
+$(document).ready(function() {
+  $('#v1').hide();
+  $('#v2').hide();
+  $('#v3').hide();
+  $('#v4').hide();
+  $('#v5').hide();
+  $('#v6').hide();
+  $('#v7').hide();
+
+var vid1 = document.getElementById("v1")
+var vid2 = document.getElementById("v2")
+var vid3 = document.getElementById("v3")
+var vid4 = document.getElementById("v4")
+var vid5 = document.getElementById("v5")
+var vid6 = document.getElementById("v6")
+var vid7 = document.getElementById("v7")
+
+
+$('#spot1').mouseover(function() {
+	$('#v1').show().trigger('play')})
+$('#spot1').mouseout(function() {
+	$('#v1').hide().trigger('pause')})
+
+$('#spot2').mouseover(function() {
+	$('#v2').show().trigger('play')})
+$('#spot2').mouseout(function() {
+	$('#v2').hide().trigger('pause')})
+
+$('#spot3').mouseover(function() {
+	$('#v3').show().trigger('play')})
+$('#spot3').mouseout(function() {
+	$('#v3').hide().trigger('pause')})
+
+$('#spot4').mouseover(function() {
+	$('#v4').show().trigger('play')})
+$('#spot4').mouseout(function() {
+	$('#v4').hide().trigger('pause')})
+
+$('#spot5').mouseover(function() {
+	$('#v5').show().trigger('play')})
+$('#spot5').mouseout(function() {
+	$('#v5').hide().trigger('pause')})
+
+$('#spot6').mouseover(function() {
+	$('#v6').show().trigger('play')})
+$('#spot6').mouseout(function() {
+	$('#v6').hide().trigger('pause')})
+
+$('#spot7').mouseover(function() {
+	$('#v7').show().trigger('play')})
+$('#spot7').mouseout(function() {
+	$('#v7').hide().trigger('pause')})
+
+});
+
+
+
+
--- sketch.js
+++ sketch.js
@@ -1,26 +1,71 @@
-//FROM PROCESSING.ORG (https://processing.org/examples/clock.html)
-
+//ADAPTED FROM PROCESSING.ORG (https://processing.org/examples/clock.html)
+var clockShape = [];
 function setup() {
   
-  createCanvas(640, 360);
+  var clock = createCanvas(800, 800);
+  clock.className = "circle";
   stroke(255);
   
   radius = min(width, height) / 2;
   secondsRadius = radius * 0.72;
   minutesRadius = radius * 0.60;
   hoursRadius = radius * 0.50;
-  clockDiameter = radius * 1.8;
+  clockDiameter = radius * 1.6;
   
   cx = width / 2;
   cy = height / 2;
+
+///////DIV NAME (I.E. DIV10) CORRESPONDS TO//////////////////
+///////THE HOUR IT REPRESENTS ON THE CLOCK////////////////// 
+
+  var div6 = createDiv('');
+  div6.id='spot7';
+  // div6.style('background-color','black');
+  div6.position(1180,614);
+
+  var div7 = createDiv('');
+  div7.id='spot6';
+  // div7.style('background-color','black');
+  div7.position(1036.00000000000006, 576.415316289918);
+
+  var div8 = createDiv('');
+  div8.id='spot5';
+  div8.class='spots';
+  // div8.style('background-color','black');
+  div8.position(930.58468371008166, 470);
+
+  var div9 = createDiv('');
+  div9.id='spot4';
+  // div9.style('background-color','black');
+  div9.position(892, 326.00000000000006);
+
+  var div10 = createDiv('');
+  div10.id='spot3';
+  // div10.style('background-color','black');
+  div10.position(930.5846837100817, 182.99999999999997);
+
+  var div11 = createDiv('');
+  div11.id='spot2';
+  // div11.style('background-color','black');
+  div11.position(1035.9999999999999, 76.58468371008175);
+
+  var div12 = createDiv('');
+  div12.id='spot1';
+  // div12.style('background-color','black');
+  div12.position(1179.99999999999994,38);
 }
 
 function draw() {
-  background(0);
+  // background(0);
   
   // Draw the clock background
-  fill(80);
-  noStroke();
+  // colorMode(HSB, 255);
+  // c = color(0, 10, 180);
+  // fill(c);
+  // noFill(220, 220, 220, 50);
+  noFill();
+  clear();
+  stroke(255);
   ellipse(cx, cy, clockDiameter, clockDiameter);
   
   // Angles for sin() and cos() start at 3 o'clock;
@@ -39,13 +84,17 @@ function draw() {
   line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
   
   // Draw the minute ticks
-  strokeWeight(2);
-  beginShape(POINTS);
-  for (a = 0; a < 360; a+=6) {
-  angle = radians(a);
-  x = cx + cos(angle) * secondsRadius;
-  y = cy + sin(angle) * secondsRadius;
-    vertex(x, y);
-  }
-  endShape();
+  fill('red');
+  noStroke();
+  // stroke('rgb(100%,0%,10%)');
+  // strokeWeight(0);
+
+  var clock6 = ellipse (400,688,20,20);
+  var clock7 = ellipse (256.00000000000006, 649.415316289918,20,20);
+  var clock8 = ellipse (150.58468371008166, 544,20,20);
+  var clock11 = ellipse (255.9999999999999, 150.58468371008175,20,20);
+  var clock12 = ellipse (399.99999999999994,112,20,20);
+  var clock9 = ellipse (112, 400.00000000000006,20,20);
+  var clock10 = ellipse (150.5846837100817, 255.99999999999997,20,20);
+
 }
